ed704f84a7303843f35f4e199642a77d1a7d3da6
𝐓ᴇᴀᴍ 𝐋ᴏꜰɪ <niterix19@gmail.com>
2024-11-01 09:05:17 -0700

    Update info.py

--- DAXXMUSIC/plugins/misc/info.py
+++ DAXXMUSIC/plugins/misc/info.py
@@ -10,11 +10,11 @@ from typing import Union, Optional
 import random
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://files.catbox.moe/ua2nnx.jpg",
+    "https://files.catbox.moe/40z6oq.jpg",
+    "https://files.catbox.moe/8zq7wq.jpg",
+    "https://files.catbox.moe/c7zsf6.jpg",
+    "https://files.catbox.moe/15rwnp.jpg",
 ]
 
 # --------------------------------------------------------------------------------- #
